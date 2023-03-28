56e7db0b0c0d6cf42e6fb48bbc50d4e7c12f6f58
Wafula Nasombi <71706858+WafulaNasombi@users.noreply.github.com>
2023-03-28 06:05:56 +0300

    Add files via upload

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,9 +1,113 @@
 import React from 'react'
 
+import  { useState } from 'react';
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+  const handleFacebookLogin = () => {
+    // Handle Facebook login logic here
+  };
+
+  const handleGoogleLogin = () => {
+    // Handle Google login logic here
+  };
+
   return (
-    <div>register</div>
-  )
+    <div>
+      <form onSubmit={handleLoginFormSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+        <button type="submit">Login</button>
+      </form>
+      <button onClick={handleFacebookLogin}>Login with Facebook</button>
+      <button onClick={handleGoogleLogin}>Login with Google</button>
+      <div>
+        <p>Don't have an account?</p>
+        <button>Register</button>
+      </div>
+    </div>
+  );
+};
+
+const RegistrationForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [accountType, setAccountType] = useState("");
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleAccountTypeChange = (e) => {
+    setAccountType(e.target.value);
+  };
+
+  const handleRegistrationFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle registration logic here
+  };
+
+  const passwordType = passwordVisible ? "text" : "password";
+
+  return (
+    <div>
+      <form onSubmit={handleRegistrationFormSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" value={firstName} onChange={handleFirstNameChange} />
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" value={lastName} onChange={handleLastNameChange} />
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={handleUsernameChange} />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} />
+        <label htmlFor="password">Password:</label>
+        <input type={passwordType} id="password" value={password} onChange={handlePasswordChange}/>
+      </form>
+      </div>
+  );
 }
 
-export default Register
+export default Register;
+
+  
