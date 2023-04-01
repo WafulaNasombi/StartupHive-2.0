edba61abb0eefbb2ddba30da673913fb98238052
Wafula Nasombi <71706858+WafulaNasombi@users.noreply.github.com>
2023-04-01 20:29:52 +0300

    Update developerDashboard.jsx

--- client/src/components/developerDashboard.jsx
+++ client/src/components/developerDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, Alert } from "reactstrap";
 
+
 const DeveloperProfile = () => {
   const [portfolio, setPortfolio] = useState("");
   const [projectTitle, setProjectTitle] = useState("");
@@ -43,58 +44,63 @@ const DeveloperProfile = () => {
   };
 
   return (
-    <div>
+    <div  className="container mt-5" style={{ background: "#3F3D56", color: "white", padding: "20px", borderRadius: "10px"}}>
       {alert && <Alert color="success">{alert}</Alert>}
-      <h2>Developer Profile</h2>
+      <h2 style={{ marginBottom: "20px" }}>Developer Profile</h2>
       <Form onSubmit={handleFormSubmit}>
         <FormGroup>
-          <Label for="portfolio">Portfolio</Label>
+          <Label for="portfolio" style={{ marginBottom: "5px" }}>Portfolio</Label>
           <Input
             type="textarea"
             name="portfolio"
             id="portfolio"
             value={portfolio}
             onChange={handlePortfolioChange}
+            style={{ background: "#514E6A", color: "white" }}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="projectTitle">Project Title</Label>
+          <Label for="projectTitle" style={{ marginBottom: "5px" }}>Project Title</Label>
           <Input
             type="text"
             name="projectTitle"
             id="projectTitle"
             value={projectTitle}
             onChange={handleProjectTitleChange}
+            style={{ background: "#514E6A", color: "white" }}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="projectDescription">Project Description</Label>
+          <Label for="projectDescription" style={{ marginBottom: "5px" }}>Project Description</Label>
           <Input
             type="textarea"
             name="projectDescription"
             id="projectDescription"
             value={projectDescription}
             onChange={handleProjectDescriptionChange}
+            style={{ background: "#514E6A", color: "white" }}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="projectFiles">Project Files</Label>
+          <Label for="projectFiles" style={{ marginBottom: "5px" }}>Project Files</Label>
           <Input
             type="file"
             name="projectFiles"
             id="projectFiles"
             multiple
             onChange={handleProjectFileChange}
+            style={{ background: "#514E6A", color: "white" }}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="stakeOffer">Stake Offer</Label>
+          <Label for="stakeOffer" style={{ marginBottom: "5px" }}>Stake Offer</Label>
           <Input
             type="text"
             name="stakeOffer"
             id="stakeOffer"
             value={stakeOffer}
             onChange={handleStakeOfferChange}
+            style={{ background: "#514E6A", color: "white" }}
           />
         </FormGroup>
       </Form>
